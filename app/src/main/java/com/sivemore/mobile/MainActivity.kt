452ea8ea9fa77cd5da2a1ca02ec45f75d22032bc
Emliano520 <20243ds133@utez.edu.mx>
2026-04-16 07:30:34 -0600

    sisi

--- app/src/main/java/com/sivemore/mobile/MainActivity.kt
+++ app/src/main/java/com/sivemore/mobile/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
@@ -18,11 +19,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            SivemoreTheme {
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    SivemoreApp()
-                }
-            }
+            if (isFinishing || isDestroyed) return@setContent
+            MainActivityContent()
+        }
+    }
+}
+
+@Composable
+private fun MainActivityContent() {
+    SivemoreTheme {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            SivemoreApp()
         }
     }
 }
